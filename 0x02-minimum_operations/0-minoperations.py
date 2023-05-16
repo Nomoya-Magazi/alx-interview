f0c973406b8e268fd40c919b13a37f3409ac3e80
Nomoya Magazi <nomoya.magazi@gmail.com>
2023-05-16 13:13:59 +0000

    corrected the formula

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,34 @@
-#!/usr/bin/python3
-
-'''Computes the fewest number of operations needed to result
-in exactly n H characters.
+mum operations coding challenge.
 '''
 
 
 def minOperations(n):
-    if n == 1:
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
         return 0
-    operations = 0
-    currHs = 1
-    buffer = 0
-    while currHs < n:
-        if n % currHs == 0:
-            buffer = currHs
-        currHs += buffer
-        operations += 1
-    return operations
+    count = 0
+    clip = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clip == 0:
+            # init (the first copy all and paste)
+            clip = done
+            done += clip
+            count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clip = done
+            done += clip
+            count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clip > 0:
+            # paste
+            done += clip
+            count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return count
